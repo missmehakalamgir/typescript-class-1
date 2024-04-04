a19c379697a361de3fd64cebeb629e532ecbd7f9
Mehak Alamgir <159041130+missmehakalamgir@users.noreply.github.com>
2024-04-04 12:36:53 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,48 @@
+//Topic's For Class 1
+
+//hello world
+//comments
+//variables
+//datatypes
+//template literals
+
+//program 1 hello world 
+
+//console.log("hello world")
+
+//program 2 comments
+
+//console.log("welcome to the typescript class 1 we cover 4 topics today")
+//variable , datatypes , inquirer
+
+
+//topic 3 datatypes
+
+//variables
+//let x1 = "Mehak Alamgir"
+//console.log(x1)
+
+//numeric datatypes, string datatype, boolean datatypes, symbol
+
+//void = 0 
+//undefined = empty
+
+
+
+//numeric datatypes (numbers)
+
+//let box : number = 10 //quantity of pencils
+//console.log(box)
+
+//string datatypes 
+
+//let name1: string = "typescript"
+//console.log(name1)
+
+
+//boolean datatypes (true/false)
+//const myBool: boolean = true
+//console.log(myBool)
+
+
+//windows.alert output in javascript
